8cd200c2c119d8136fee65ee2af241ed28d7da79
Joaquin <JoaquinJSX>
2025-04-05 18:41:26 -0500

    Help me!

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { lazy, Suspense } from "react";
 const Dashboard = lazy(() => import("./AppComponents/Dashboard"));
@@ -43,7 +43,12 @@ export default function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/log-in" element={<LogIn users={users} setUserLoggedIn={setUserLoggedIn} />} />
             <Route path="/sign-up" element={<SignUp users={users} setUsers={setUsers} setUserLoggedIn={setUserLoggedIn} />} />
-            <Route path="/finantial_control" element={<FinantialControl users={users} setUsers={setUsers} userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />} />
+            <Route
+              path="/finantial_control"
+              element={userLoggedIn ? <FinantialControl users={users} setUsers={setUsers} userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+                :
+                <Navigate to="/log-in" replace />}
+            />
           </Routes>
         </Suspense>
       </BrowserRouter>
